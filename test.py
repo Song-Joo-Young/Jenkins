4c2e1c5cbd79dd45c542036d98186e22ddb0295e
jysong <Jooyoung.song@baum-ds.com>
2025-02-28 17:29:22 +0900

    test pass/fail

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 # test.py
 
-def add(a, b):
-    return a + b
+def test_addition():
+    assert 1 + 1 == 2  # True이므로 Pass
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(2, 3) == 5
+def test_subtraction():
+    assert 2 - 1 == 1  # True이므로 Pass
+
+def test_multiplication():
+    assert 2 * 2 == 5  # False이므로 Fail
+
+def test_division():
+    assert 10 / 2 == 5  # True이므로 Pass
